9c4bbf39abb22463b414828243f48be4ec541c64
Romain Francois <romain@purrple.cat>
2018-11-15 21:38:42 +0100

    steal at load time

--- R/compat-dplyr.R
+++ R/compat-dplyr.R
@@ -1,11 +1,10 @@
-steal <- function(pkg, fun){
-  eval(call(":::", sym(pkg), sym(fun)))
-}
+apply_filter_syms <- function(...) abort("")
+tbl_at_syms <- function(...) abort("")
+tbl_if_syms <- function(...) abort("")
 
-quo_reduce <- steal("dplyr", "quo_reduce")
-all_exprs <- steal("dplyr", "all_exprs")
-any_exprs <- steal("dplyr", "any_exprs")
-expr_substitute <- steal("dplyr", "any_exprs")
-apply_filter_syms <- steal("dplyr", "apply_filter_syms")
-tbl_at_syms <- steal("dplyr", "tbl_at_syms")
-tbl_if_syms <- steal("dplyr", "tbl_if_syms")
+.onLoad <- function(...) {
+  dplyr <- asNamespace("dplyr")
+  apply_filter_syms <<- dplyr$apply_filter_syms
+  tbl_at_syms <<- dplyr$tbl_at_syms
+  tbl_if_syms <<- dplyr$tbl_if_syms
+}
